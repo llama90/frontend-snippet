818e8206cf2af6830ee059cddb98024c4e2af08d
Hyunseok Seo <hsseo0501@gmail.com>
2020-11-09 16:04:36 +0900

    Add function to create new task

--- vue/todo/src/App.vue
+++ vue/todo/src/App.vue
@@ -13,6 +13,13 @@
                 <div class="col-2 text-center">
                     <input type="checkbox" v-model="t.done" class="form-check-input"/></div>
             </div>
+            <div class="row py-2">
+                <div class="col">
+                    <input v-model="newItemText" class="form-control"/></div>
+                <div class="col-2">
+                    <button class="btn btn-primary" v-on:click="addNewTodo">Add</button>
+                </div>
+            </div>
             <div class="row bg-secondary py-2 mt-2 text-white">
                 <div class="col text-center">
                     <input type="checkbox" v-model="hideCompleted" class="form-check-input"/> <label
@@ -34,12 +41,21 @@
                     {action: "Study Backend", done: false},
                     {action: "Study DevOps", done: false},
                     {action: "Study Machine Learning", done: true}],
-                hideCompleted: true
+                hideCompleted: true,
+                newItemText: ""
             }
         },
         computed: {
             filteredTasks() {
                 return this.hideCompleted ? this.tasks.filter(t => !t.done) : this.tasks
+            }
+        },
+        methods: {
+            addNewTodo() {
+                this.tasks.push({
+                    action: this.newItemText, done: false
+                });
+                this.newItemText = "";
             }
         }
     }
