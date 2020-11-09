335958e36f27cc00b956277d97e2372594e80325
Hyunseok Seo <hsseo0501@gmail.com>
2020-11-09 18:25:03 +0900

    Add function to clear complete tasks

--- vue/todo/src/App.vue
+++ vue/todo/src/App.vue
@@ -4,15 +4,21 @@
             {{name}}'s To Do List
         </h4>
         <div class="container-fluid p-4">
-            <div class="row">
-                <div class="col font-weight-bold">Task</div>
-                <div class="col-2 font-weight-bold">Done</div>
+            <div class="row" v-if="filteredTasks.length == 0">
+                <div class="col text-center">
+                    <b>Nothing to do. Hurrah!</b></div>
             </div>
-            <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
-                <div class="col">{{t.action}}</div>
-                <div class="col-2 text-center">
-                    <input type="checkbox" v-model="t.done" class="form-check-input"/></div>
-            </div>
+            <template v-else>
+                <div class="row">
+                    <div class="col font-weight-bold">Task</div>
+                    <div class="col-2 font-weight-bold">Done</div>
+                </div>
+                <div class="row" v-for="t in filteredTasks" v-bind:key="t.action">
+                    <div class="col">{{t.action}}</div>
+                    <div class="col-2 text-center">
+                        <input type="checkbox" v-model="t.done" class="form-check-input"/></div>
+                </div>
+            </template>
             <div class="row py-2">
                 <div class="col">
                     <input v-model="newItemText" class="form-control"/></div>
@@ -26,6 +32,11 @@
                         class="form-check-label font-weight-bold">
                     Hide completed tasks
                 </label>
+                </div>
+                <div class="col text-center">
+                    <button class="btn btn-sm btn-warning"
+                            v-on:click="deleteCompleted"> Delete Completed
+                    </button>
                 </div>
             </div>
         </div>
@@ -55,8 +66,15 @@
                 this.tasks.push({
                     action: this.newItemText, done: false
                 });
-                localStorage.setItem("todos", JSON.stringify(this.tasks));
+                this.storeData();
                 this.newItemText = "";
+            },
+            storeData() {
+                localStorage.setItem("todos", JSON.stringify(this.tasks));
+            },
+            deleteCompleted() {
+                this.tasks = this.tasks.filter(t => !t.done);
+                this.storeData();
             }
         },
         created() {
