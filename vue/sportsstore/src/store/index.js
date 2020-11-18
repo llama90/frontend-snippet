2f42f09b96bc3a5dd44def4f61d0c773e861f853
Hyunseok Seo <hsseo0501@gmail.com>
2020-11-18 15:58:17 +0900

    Add product store page
      - Store.vue: view product store page by containing other components
      - ProductList.vue:  view product list and contain a filter to price
      - PageControls.vue: handle pagination
      - CategoryControls.vue: handle category

--- vue/sportsstore/src/store/index.js
+++ vue/sportsstore/src/store/index.js
@@ -14,6 +14,36 @@ for (let i = 1; i <= 10; i++) {
 export default new Vuex.Store({
     strict: true,
     state: {
-        products: testData
+        products: testData,
+        productsTotal: testData.length,
+        currentPage: 1,
+        pageSize: 4,
+        currentCategory: "All"
+    },
+    getters: {
+        productsFilteredByCategory: state => state.products
+            .filter(p => state.currentCategory == "All" || p.category == state.currentCategory),
+        processedProducts: (state, getters) => {
+            let index = (state.currentPage - 1) * state.pageSize;
+            return getters.productsFilteredByCategory
+                .slice(index, index + state.pageSize);
+        },
+        pageCount: (state, getters) =>
+            Math.ceil(getters.productsFilteredByCategory.length / state.pageSize),
+        categories: state => ["All",
+            ...new Set(state.products.map(p => p.category).sort())]
+    },
+    mutations: {
+        setCurrentPage(state, page) {
+            state.currentPage = page;
+        },
+        setPageSize(state, size) {
+            state.pageSize = size;
+            state.currentPage = 1;
+        },
+        setCurrentCategory(state, category) {
+            state.currentCategory = category;
+            state.currentPage = 1;
+        }
     }
 })
