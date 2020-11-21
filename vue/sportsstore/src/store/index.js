1ea57f8961150cede0de7bb1c2a7bae80f0f12f6
Hyunseok Seo <hsseo0501@gmail.com>
2020-11-21 15:39:04 +0900

    Add administration feature
      - An administrator feature adds to manage products and orders.
      When the user was order completed, the order sheet can be confirmed.

--- vue/sportsstore/src/store/index.js
+++ vue/sportsstore/src/store/index.js
@@ -5,6 +5,7 @@ import Axios from "axios";
 
 import CartModule from "./cart";
 import OrdersModule from "./orders";
+import AuthModule from "./auth";
 
 Vue.use(Vuex);
 
@@ -22,7 +23,7 @@ for (let i = 1; i <= 10; i++) {
 
 export default new Vuex.Store({
     strict: true,
-    modules: {cart: CartModule, orders: OrdersModule},
+    modules: {cart: CartModule, orders: OrdersModule, auth: AuthModule},
     state: {
         // products: [],
         categoriesData: [],
