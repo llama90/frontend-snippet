accde7a547150611a8b36908e98e85d617f3f536
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-04 16:23:39 +0900

    Add and modify files to understand static content

--- react/projecttools/src/AppJavascript.js
+++ react/projecttools/src/AppJavascript.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import "./App.css";
+import reactLogo from "./logo.svg";
 
 let name = "Lucas";
 const country = "Korea";
@@ -7,7 +9,13 @@ export default class extends Component {
   message = () => `Hello ${name} from ${country}`;
 
   render = () =>
-    <h4 className="bg-primary text-white text-center p-3">
-      {this.message()}
-    </h4>
+    <div className="text-center">
+      <h4 className="bg-primary text-white text-center p-3">
+        {this.message()}
+      </h4>
+      <img src={reactLogo} alt="reactLogo"/>
+      <link rel="stylesheet"
+            href={process.env.PUBLIC_URL + "/static.css"}/>
+      <img src={process.env.PUBLIC_URL + "/logo.svg"} alt="reactLogo"/>
+    </div>
 }
