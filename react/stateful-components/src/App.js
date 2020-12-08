c536321f1351041bca5147153bb9d69afe1a86d1
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-08 21:36:13 +0900

    Add components for lifting up state data

--- react/stateful-components/src/App.js
+++ react/stateful-components/src/App.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 import {TableCell} from "./component/TableCell";
+import {TableCellLiftingUpStateData} from "./component/TableCellLiftingUpStateData";
 
 let names = ["Bob", "Alice", "Dora"]
+
+let namesStateToParent = ["Bob", "Alice", "Dora"]
 
 function reverseNames() {
   names.reverse();
@@ -15,7 +18,10 @@ function promoteName(name) {
   ReactDOM.render(<App/>, document.getElementById('root'));
 }
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const incrementCounter = (increment) => setCounter(counter + increment);
+
   return <React.StrictMode>
     <div>
       <h1 className="bg-primary text-white text-center p-2"> Rendering Multiple Elements
@@ -45,7 +51,35 @@ function App() {
       </table>
       )
     </div>
+    <div>
+      <h1 className="bg-primary text-white text-center p-2"> State Data To Parent
+      </h1>
+      <table className="table table-sm table-striped">
+        <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Letters</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+          names.map(
+            (namesStateToParent, index) =>
+              <tr key={namesStateToParent}>
+                <TableCellLiftingUpStateData
+                  index={index}
+                  name={namesStateToParent}
+                  reverseCallback={reverseNames}
+                  promoteCallback={promoteName}
+                  counter={counter}
+                  incrementCallback={incrementCounter}/>
+              </tr>
+          )
+        }
+        </tbody>
+      </table>
+      )
+    </div>
   </React.StrictMode>
 }
-
-export default App;
