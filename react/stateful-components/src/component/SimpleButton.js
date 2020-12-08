7cb1c056159cf997e6f9d1d9186b62a844c96d08
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-08 09:55:41 +0900

    Modify component to understand stateful component

--- react/stateful-components/src/component/SimpleButton.js
+++ react/stateful-components/src/component/SimpleButton.js
@@ -1,10 +1,90 @@
-import React from "react";
+import React, {Component} from "react";
 
-export function SimpleButton(props) {
-  return (
-    <button onClick={props.callback} className={props.className}
-            disabled={props.disabled === "true" || props.disabled === true}>
-      {props.text}
-    </button>
-  )
+export class SimpleButton extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      hasButtonBeenClicked: false
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}
+              className={this.props.className}
+              disabled={this.props.disabled === "true" || this.props.disabled === true}>
+        {this.props.text}
+        {this.state.counter}
+        {this.state.hasButtonBeenClicked &&
+        <div>Button Clicked!</div>
+        }
+      </button>)
+  }
+
+  handleClick = () => {
+    for (let i = 0; i < 5; i++) {
+      this.setState((state, props) => {
+        return {counter: state.counter + 1}
+      });
+    }
+    this.setState({hasButtonBeenClicked: true});
+    this.props.callback();
+  }
+
+  // Modify state data
+  // handleClick = () => {
+  //   this.setState({
+  //     counter: this.state.counter + 1,
+  //     hasButtonBeenClicked: true
+  //   });
+  //   this.props.callback();
+  // }
+
+  // Perform related state changes
+  // handleClick = () => {
+  //   this.setState({
+  //     counter: this.state.counter + 1,
+  //-     hasButtonBeenClicked: true
+  //+     hasButtonBeenClicked: this.state.counter > 0
+  //   });
+  //   this.props.callback();
+  // }
+
+  // Make dependent updates
+  // handleClick = () => {
+  //-   counter: this.state.counter + 1,
+  //-   hasButtonBeenClicked: this.state.counter > 0
+  //+   this.setState({counter: this.state.counter + 1});
+  //+   this.setState({hasButtonBeenClicked: this.state.counter > 0});
+  //   this.props.callback();
+  // }
+
+  // Using a callback
+  // handleClick = () => {
+  //-   this.setState({counter: this.state.counter + 1});
+  //-   this.setState({hasButtonBeenClicked: this.state.counter > 0});
+  //+   this.setState({counter: this.state.counter + 1},
+  //+     () => this.setState({hasButtonBeenClicked: this.state.counter > 0}));
+  //   this.props.callback();
+  // }
+
+  // Make multiple updates: perform the effect of repeatedly modifying the same property
+  // handleClick = () => {
+  //   for (let i = 0; i < 5; i++) {
+  //     this.setState({counter: this.state.counter + 1});
+  //   }
+  //   this.setState({hasButtonBeenClicked: true});
+  //   this.props.callback();
+  // }
+
+  // Make multiple updates: perform multiple updates and have each take effect in sequence
+  // handleClick = () => {
+  //   for (let i = 0; i < 5; i++) {
+  //     this.setState((state, props) => {return{counter: state.counter + 1}});
+  //   }
+  //   this.setState({hasButtonBeenClicked: true});
+  //   this.props.callback();
+  // }
 }
