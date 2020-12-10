50b1f7bc60ef49d50704de97b444639d2c1319eb
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-10 09:55:16 +0900

    Add a component to understand an event propagation

--- react/reactevents/src/App.js
+++ react/reactevents/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {ThemeButton} from "./component/ThemeButton";
 
 export default class App extends Component {
   constructor(props) {
@@ -9,6 +10,13 @@ export default class App extends Component {
       counter: 0,
       theme: "secondary"
     }
+  }
+
+  selectTheme = (newTheme) => {
+    this.setState({
+      theme: newTheme,
+      message: `Theme: ${newTheme}`
+    });
   }
 
   handleEvent = () => {
@@ -47,6 +55,12 @@ export default class App extends Component {
     }
   }
 
+  handleClick = (event) => {
+    console.log(`App: Type: ${event.type} `
+      + `Target: ${event.target.tagName} `
+      + `CurrentTarget: ${event.currentTarget.tagName}`);
+  }
+
   render() {
     return (
       <div className="m-2">
@@ -79,21 +93,30 @@ export default class App extends Component {
 
         <div className="m-2">
           <div className="form-check">
-            <input className="form-check-input" type="checkbox"
-                   onClick={this.toggleCheckBox}/> <label>This is a checkbox</label>
+            <input className="form-check-input" type="checkbox" onClick={this.toggleCheckBox}/>
+            <label>This is a checkbox</label>
           </div>
           <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
             {this.state.messageWithCustomArgument} </div>
           <div className="text-center">
-            <button className="btn btn-primary"
-                    onClick={this.handleEventCustomArgument}>
-              Normal
-            </button>
-            <button className="btn btn-danger m-1" onClick={this.handleEventCustomArgument}>
-              Danger
-            </button>
+            <button className="btn btn-primary" onClick={this.handleEventCustomArgument}>Normal</button>
+            <button className="btn btn-danger m-1" onClick={this.handleEventCustomArgument}>Danger</button>
           </div>
         </div>
+
+        <div className="m-2" onClick={this.handleClick}>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" onClick={this.toggleCheckBox}/>
+            <label>This is a checkbox</label>
+          </div>
+          <div className={`h4 bg-${this.state.theme} text-white text-center p-2`}>
+            {this.state.messageWithCustomArgument} </div>
+          <div className="text-center" onClick={this.handleClick}>
+            <ThemeButton theme="primary" callback={this.selectTheme}/>
+            <ThemeButton theme="danger" callback={this.selectTheme}/>
+          </div>
+        </div>
+
       </div>
 
     )
