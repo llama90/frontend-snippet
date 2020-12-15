04fb270744e1d9ad2706671f052c65347c8d94dd
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-15 09:56:39 +0900

    Prepare the Project
      npx create-react-app composition
      cd composition
      npm install bootstrap
      create some components

---
+++ react/composition/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Message} from "./component/Message";
+import {ActionButton} from './component/ActionButton';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({counter: this.state.counter + 1});
+  }
+
+  render() {
+    return <div className="m-2 text-center">
+      <Message theme="primary" message={`Counter: ${this.state.counter}`}/>
+      <ActionButton theme="secondary" text="Increment" callback={this.incrementCounter}/>
+    </div>
+  }
+}
