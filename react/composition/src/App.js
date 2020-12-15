066376a5d2b6815720a643a81d634a102ef7dafb
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-16 06:03:35 +0900

    Add component for specialized component

--- react/composition/src/App.js
+++ react/composition/src/App.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import {Message} from "./component/Message";
 import {ActionButton} from './component/ActionButton';
 import {ThemeSelector} from './component/ThemeSelector';
+import {GeneralList} from './component/GeneralList';
+import {SortedList} from "./component/SortedList";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counter: 0,
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
     }
   }
 
@@ -21,6 +24,16 @@ export default class App extends Component {
         <Message theme="primary" message={`Counter: ${this.state.counter}`}/>
         <ActionButton theme="secondary" text="Increment" callback={this.incrementCounter}/>
       </ThemeSelector>
+      <div className="container-fluid m-2">
+        <div className="row">
+          <div className="col-6">
+            <GeneralList list={this.state.names} theme="primary"/>
+          </div>
+          <div className="col-6">
+            <SortedList list={this.state.names}/>
+          </div>
+        </div>
+      </div>
     </div>
   }
 }
