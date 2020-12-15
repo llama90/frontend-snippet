49f999e4f8433f298e4e6c254601446f22040061
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-16 06:19:40 +0900

    Add component for higher order

--- react/composition/src/App.js
+++ react/composition/src/App.js
@@ -4,18 +4,30 @@ import {ActionButton} from './component/ActionButton';
 import {ThemeSelector} from './component/ThemeSelector';
 import {GeneralList} from './component/GeneralList';
 import {SortedList} from "./component/SortedList";
+import {ProFeature} from "./component/ProFeature";
+import {ProController} from "./component/ProController";
+import {LogToConsole} from "./component/LogToConsole";
+
+const ProList = ProFeature(SortedList);
+const ProControllerList = ProController(LogToConsole(SortedList, "Sorted", true, true, true));
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       counter: 0,
-      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
+      cities: ["London", "New York", "Paris", "Milan", "Boston"],
+      proMode: false
     }
   }
 
   incrementCounter = () => {
     this.setState({counter: this.state.counter + 1});
+  }
+
+  toggleProMode = () => {
+    this.setState({proMode: !this.state.proMode});
   }
 
   render() {
@@ -26,12 +38,46 @@ export default class App extends Component {
       </ThemeSelector>
       <div className="container-fluid m-2">
         <div className="row">
-          <div className="col-6">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <input type="checkbox" className="form-check-input"
+                     value={this.state.proMode}
+                     onChange={this.toggleProMode}/>
+              <label className="form-check-label">Pro Mode</label>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-3">
             <GeneralList list={this.state.names} theme="primary"/>
           </div>
-          <div className="col-6">
-            <SortedList list={this.state.names}/>
+          <div className="col-3">
+            <ProList list={this.state.names}
+                     pro={this.state.proMode}/>
           </div>
+          <div className="col-3">
+            <GeneralList list={this.state.cities} theme="secondary"/>
+          </div>
+          <div className="col-3">
+            <ProList list={this.state.cities}
+                     pro={this.state.proMode}/>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+          <GeneralList list={this.state.names} theme="primary"/>
+        </div>
+        <div className="col-3">
+          <ProControllerList list={this.state.names}/>
+        </div>
+        <div className="col-3">
+          <GeneralList list={this.state.cities} theme="secondary"/>
+        </div>
+        <div className="col-3">
+          <ProControllerList list={this.state.cities}/>
         </div>
       </div>
     </div>
