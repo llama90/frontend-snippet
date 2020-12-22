72e012b9798f67e6d29108c23f6b65310b746fe5
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-22 21:41:15 +0900

    Prepare the Project
      - npx create-react-app refs
      - cd refs
      - npm install bootstrap
      - npm install jquery
      - create some components

---
+++ react/refs/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import {Editor} from "./component/Editor"
+import {ProductTable} from "./component/ProductTable";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    }
+  }
+
+  addProduct = (product) => {
+    if (this.state.products.indexOf(product.name) === -1) {
+      this.setState({products: [...this.state.products, product]});
+    }
+  }
+
+  render() {
+    return <div>
+      <Editor callback={this.addProduct}/>
+      <h6 className="bg-secondary text-white m-2 p-2">Products</h6>
+      <div className="m-2">
+        {
+          this.state.products.length === 0
+            ? <div className="text-center">No Products</div>
+            : <ProductTable products={this.state.products}/>
+        }
+      </div>
+    </div>
+  }
+}
