e081280c3cdf9c8b3714bf3fecaf452d47182313
Hyunseok Seo <hsseo0501@gmail.com>
2020-12-23 20:00:51 +0900

    Add validating uncontrolled form components

--- react/refs/src/component/Editor.js
+++ react/refs/src/component/Editor.js
@@ -1,12 +1,26 @@
 import React, {Component} from "react";
+import {ValidationDisplay} from "./ValidationDisplay";
+import {GetValidationMessages} from "./ValidationMessages";
 
 export class Editor extends Component {
   constructor(props) {
     super(props);
     this.formElements = {
-      name: {label: "Name", name: "name"},
-      category: {label: "Category", name: "category"},
-      price: {label: "Price", name: "price"}
+      name: {
+        label: "Name", name: "name",
+        validation: {required: true, minLength: 3}
+      },
+      category: {
+        label: "Category", name: "category",
+        validation: {required: true, minLength: 5}
+      },
+      price: {
+        label: "Price", name: "price",
+        validation: {type: "number", required: true, min: 5}
+      }
+    }
+    this.state = {
+      errors: {}
     }
   }
 
@@ -17,14 +31,31 @@ export class Editor extends Component {
   }
 
   handleAdd = () => {
-    let data = {};
-    Object.values(this.formElements)
-      .forEach(v => {
-        data[v.element.name] = v.element.value;
-        v.element.value = "";
-      });
-    this.props.callback(data);
-    this.formElements.name.element.focus();
+    if (this.validateFormElements()) {
+      let data = {};
+      Object.values(this.formElements)
+        .forEach(v => {
+          data[v.element.name] = v.element.value;
+          v.element.value = "";
+        });
+      this.props.callback(data);
+      this.formElements.name.element.focus();
+    }
+  }
+
+  validateFormElement = (name) => {
+    let errors = GetValidationMessages(this.formElements[name].element);
+    this.setState(state => state.errors[name] = errors);
+    return errors.length === 0;
+  }
+  validateFormElements = () => {
+    let valid = true;
+    Object.keys(this.formElements).forEach(name => {
+      if (!this.validateFormElement(name)) {
+        valid = false;
+      }
+    })
+    return valid;
   }
 
   render() {
@@ -33,7 +64,14 @@ export class Editor extends Component {
         Object.values(this.formElements).map(elem =>
           <div className="form-group p-2" key={elem.name}>
             <label>{elem.label}</label>
-            <input className="form-control" name={elem.name} autoFocus={elem.name === "name"} ref={this.setElement}/>
+            <input className="form-control"
+                   name={elem.name}
+                   autoFocus={elem.name === "name"}
+                   ref={this.setElement}
+                   onChange={() => this.validateFormElement(elem.name)}
+                   {...elem.validation} />
+            <ValidationDisplay
+              errors={this.state.errors[elem.name]}/>
           </div>
         )
       }
