a4355d14c93c99b87487aa58c4aedeb2f42fd085
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-04 19:27:25 +0900

    Add delay before dispatching actions using enhaner function
      It is not important, most projects are sufficient by middleware
      to support the behavior of the data store.

--- react/productapp/src/store/StoreAccess.js
+++ react/productapp/src/store/StoreAccess.js
@@ -9,12 +9,13 @@ export class StoreAccess extends Component {
       state: (storeState) => storeState.stateData
     }
     this.state = this.selectData();
+    this.buttonRef = React.createRef();
   }
 
   render() {
     return <React.Fragment>
       <div className="text-center">
-        <button className="btn btn-primary m-1" onClick={this.dispatchAction}>
+        <button className="btn btn-primary m-1" ref={this.buttonRef} onClick={this.dispatchAction}>
           Dispatch Action
         </button>
       </div>
@@ -27,7 +28,9 @@ export class StoreAccess extends Component {
   }
 
   dispatchAction = () => {
-    this.props.store.dispatch(resetStore())
+    this.buttonRef.current.disabled = true;
+    this.props.store.dispatchAsync(resetStore())
+      .then(data => this.buttonRef.current.disabled = false);
   }
 
   selectData() {
