8394aee9d7ee22f1c38c96cafd74c42b13e8eb0f
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-08 10:15:10 +0900

    Add programmatic navigation

--- react/productapp/src/component/Selector.js
+++ react/productapp/src/component/Selector.js
@@ -1,15 +1,37 @@
 import React, {Component} from "react";
-import {BrowserRouter as Router, Route, Switch, Redirect, withRouter} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch, Redirect, withRouter, Prompt} from "react-router-dom";
 import {ProductDisplay} from "./ProductDisplay";
 import {SupplierDisplay} from "./SupplierDisplay";
 import {RouteInfo} from "../routing/RouteInfo";
 import {ToggleLink} from "../routing/ToggleLink";
+import {CustomPrompt} from "../routing/CustomPrompt";
 
 const RouteInfoHOC = withRouter(RouteInfo)
 
 export class Selector extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPrompt: false,
+      message: "",
+      callback: () => {
+      }
+    }
+  }
+
+  customGetUserConfirmation = (message, navCallback) => {
+    this.setState({
+      showPrompt: true, message: message,
+      callback: (allow) => {
+        navCallback(allow);
+        this.setState({showPrompt: false})
+      }
+    });
+  }
+
   render() {
-    return <Router>
+    return <Router getUserConfirmation={this.customGetUserConfirmation}>
       <div className="container-fluid">
         <div className="row">
           <div className="col-2">
@@ -20,6 +42,8 @@ export class Selector extends Component {
             <ToggleLink to="/info" exact={true}>All Info</ToggleLink>
           </div>
           <div className="col">
+            <CustomPrompt show={this.state.showPrompt} message={this.state.message} callback={this.state.callback}/>
+            <Prompt message={loc => `Do you want to navigate to ${loc.pathname}`}/>
             <RouteInfoHOC/>
             <Switch>
               <Route path="/products" component={ProductDisplay}/>
