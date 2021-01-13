422a5c3556091b37d4493d09b6e3d46173481ad2
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-13 20:04:36 +0900

    Add GraphQL queries

--- react/productapp/src/graphql/resolvers.js
+++ react/productapp/src/graphql/resolvers.js
@@ -1,6 +1,25 @@
 var data = require("../../restData")();
 
+const mapIdsToProducts = (supplier, nameFilter) =>
+  supplier.products.map(id => data.products.find(p => p.id === Number(id)))
+    .filter(p => p.name.toLowerCase().includes(nameFilter.toLowerCase()));
+
 module.exports = {
   products: () => data.products,
-  suppliers: () => data.suppliers
+
+  product: ({id}) => data.products.find(p => p.id === parseInt(id)),
+
+  suppliers: () => data.suppliers.map(s => ({
+    ...s, products: ({nameFilter}) => mapIdsToProducts(s, nameFilter)
+  })),
+
+  supplier: ({id}) => {
+    const result = data.suppliers.find(s => s.id === parseInt(id));
+    if (result) {
+      return {
+        ...result,
+        products: ({nameFilter}) => mapIdsToProducts(result, nameFilter)
+      }
+    }
+  }
 }
