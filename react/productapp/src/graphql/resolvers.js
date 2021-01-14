c7fb01beb6a4f8a60e060a22c48ee2a5b4b454ee
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-14 20:05:33 +0900

    Modify graphQL schema and resolver

--- react/productapp/src/graphql/resolvers.js
+++ react/productapp/src/graphql/resolvers.js
@@ -1,4 +1,4 @@
-var data = require("../../restData")();
+var data = require("../../graphqlData")();
 
 const mapIdsToProducts = (supplier, nameFilter) =>
   supplier.products.map(id => data.products.find(p => p.id === Number(id)))
@@ -25,14 +25,13 @@ module.exports = {
     }
   },
 
-  storeProduct({product}) {
-    if (product.id == null) {
+  storeProduct(args) {
+    const product = {...args, id: Number(args.id)};
+    if (args.id == null || product.id === 0) {
       product.id = nextId++;
       data.products.push(product);
     } else {
-      product = {...product, id: Number(product.id)};
-      data.products = data.products
-        .map(p => p.id === product.id ? product : p);
+      data.products = data.products.map(p => p.id === product.id ? product : p);
     }
     return product;
   },
@@ -52,5 +51,20 @@ module.exports = {
         products: ({nameFilter}) => mapIdsToProducts(result, nameFilter)
       }
     }
+  },
+
+  deleteProduct({id}) {
+    id = Number(id);
+    data.products = data.products.filter(p => p.id !== id);
+    data.suppliers = data.suppliers.map(s => {
+      s.products = s.products.filter(p => p !== id);
+      return s;
+    })
+    return id;
+  },
+
+  deleteSupplier({id}) {
+    data.suppliers = data.suppliers.filter(s => s.id !== Number(id));
+    return id;
   }
 }
