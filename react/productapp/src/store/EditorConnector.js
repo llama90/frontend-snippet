d64c3f6c4ba0f5f822094f475e0194cc826162fb
Hyunseok Seo <hsseo0501@gmail.com>
2021-01-18 19:59:57 +0900

    Change the data store with GraphQL

--- react/productapp/src/store/EditorConnector.js
+++ react/productapp/src/store/EditorConnector.js
@@ -6,7 +6,7 @@ import {withRouter} from "react-router-dom";
 export const EditorConnector = (dataType, presentationComponent) => {
   const mapStateToProps = (storeData, ownProps) => {
     const mode = ownProps.match.params.mode;
-    const id = Number(ownProps.match.params.id);
+    const id = ownProps.match.params.id;
     return {
       editing: mode === "edit" || mode === "create",
       product: (storeData.modelData[PRODUCTS].find(p => p.id === id)) || {},
@@ -29,5 +29,6 @@ export const EditorConnector = (dataType, presentationComponent) => {
     return Object.assign({}, dataProps, routedDispatchers, ownProps);
   }
 
-  return withRouter(connect(mapStateToProps, mapDispatchToProps, mergeProps)(presentationComponent));
+  return withRouter(connect(mapStateToProps,
+    mapDispatchToProps, mergeProps)(presentationComponent));
 }
--- react/productapp/src/store/index.js
+++ react/productapp/src/store/index.js
@@ -4,25 +4,21 @@ import stateReducer from "./stateReducer";
 import {customReducerEnhancer} from "./customReducerEnhancer";
 import {multiActions} from "./multiActionMiddleware";
 import {asyncEnhancer} from "./asyncEnhancer";
-import {createRestMiddleware} from "../webservice/RestMiddleware";
+import {createGraphQLMiddleware} from "../graphql/GraphQLMiddleware";
 
 const enhancedReducer = customReducerEnhancer(
   combineReducers(
     {
       modelData: modelReducer,
       stateData: stateReducer
-    }));
-
-const restMiddleware = createRestMiddleware(
-  "http://localhost:3500/api/products",
-  "http://localhost:3500/api/suppliers");
+    })
+);
 
 export default createStore(enhancedReducer,
   compose(applyMiddleware(multiActions),
-    applyMiddleware(restMiddleware),
+    applyMiddleware(createGraphQLMiddleware()),
     asyncEnhancer(2000)));
 
 export {
   saveProduct, saveSupplier, deleteProduct, deleteSupplier
-}
-  from "./modelActionCreators";
+} from "./modelActionCreators";
